41a85dd9cf75aafb289b8934e9abcedcaefccbad
Rizqiandri07 <rizqiandri12la@gmail.com>
2026-07-06 20:09:39 +0700

    update minoritas

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,13 @@
+@props(['url'])
+
+<tr>
+<td class="header">
+    <a href="{{ $url }}" style="display: inline-block;">
+        <img
+            src="https://vdi.kinggu.site/logo-full.png"
+            alt="VDI D3TI"
+            style="max-width: 180px; height: auto;"
+        >
+    </a>
+</td>
+</tr>
